b607619457505f74c48c2184421594a302aabdbd
Noah Krohn <102704593+noah-raintree@users.noreply.github.com>
2022-08-16 12:26:10 -0400

    update userdata and create json config for tableau

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,30 @@
-#!/bin/bash -xe
+#!/bin/bash
+
+# Disable selinux
+sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
+
+# Install the AWS CLI and prerequisite packages for later
+dnf install -y unzip sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python-utils jq
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+rm -rf ./awscliv2.zip
+rm -rf ./aws
+
+# Steup the nvme share to be usable as swap and add it to fstab
+parted /dev/nvme1n1 mklabel gpt
+parted -a opt /dev/nvme1n1 mkpart primary linux-swap 0% 100%
+mkswap /dev/nvme1n1p1
+swapon /dev/nvme1n1p1
+swapid=`blkid -s PARTUUID -o value /dev/nvme1n1p1`
+fstabinfo="UUID=$swapid\tswap\tswap\tdefaults\t0\t0"
+echo -e "$fstabinfo" >> /etc/fstab
+
+# Set hostname on system
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 domain=`cat /etc/resolv.conf | grep raintreeinc.internal | awk '{ print substr( $0, 8 ) }'`
 environment=`echo $domain | cut -c 1-3`
-region=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c2- | rev`
+region=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c2- | rev`
 case $region in
   us-east-1)
     prefix="use1"
@@ -29,20 +52,40 @@ case $region in
     ;;
 esac
 servertype="tab"
-instanceid=`curl http://169.254.169.254/latest/meta-data/instance-id`
+instanceid=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id`
 shortid=${instanceid: -4}
 strhostname=$prefix$environment$servertype$shortid
 hostnamectl set-hostname $strhostname.$domain --static
-instanceID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-aws ec2 create-tags --region us-east-1 --resources $instanceID --tags Key=Name,Value=$strhostname
-aws ec2 create-tags --region us-east-1 --resources $instanceID --tags Key=ReadyForUse,Value=True
-dnf update
-dnf upgrade --refresh
-dnf install -y dnf-plugin-system-upgrade
+instanceID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+aws ec2 create-tags --region $region --resources $instanceID --tags Key=Name,Value=$strhostname
+
+# Run dnf updates
+dnf update -y
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 dnf -qy module disable postgresql
 dnf install -y postgresql13-server
 /usr/pgsql-13/bin/postgresql-13-setup initdb
 systemctl enable postgresql-13
 systemctl start postgresql-13
+
+# Join system to the domain
+objSTSToken=`curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/rt-ec2-tableau`
+export AWS_ACCESS_KEY_ID=$objSTSToken | jq -r .AccessKeyId
+export AWS_SECRET_ACCESS_KEY=$objSTSToken | jq -r .SecretAccessKey
+export AWS_DEFAULT_REGION=$region
+export AWS_SESSION_TOKEN=$objSTSToken | jq -r .Token
+objDomainInfo=aws secretsmanager get-secret-value --secret-id "domainjoin"
+user=`echo $objDomainInfo | jq -r .SecretString | jq -r .username`
+pass=`echo $objDomainInfo | jq -r .SecretString | jq -r .password`
+domain=`echo $objDomainInfo | jq -r .SecretString | jq -r .domain`
+realm discover --server-software=active-directory
+username=`echo $user | cut -d '@' -f1`
+domain_upper=`echo $user | cut -d '@' -f2 | tr '[:lower:]' '[:upper:]'`
+kuser=`echo $username@$domain_upper`
+echo $pass | kinit $kuser
+echo $pass | realm join $domain_upper -U $kuser --computer-name $strhostname --client-software=sssd
+systemctl enable sssd
+
+# Set ready tag on instance and then reboot
+aws ec2 create-tags --region $region --resources $instanceID --tags Key=ReadyForUse,Value=True
 reboot
